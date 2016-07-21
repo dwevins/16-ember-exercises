0f44886a2ffd063c22e0d21a7fb8796e10620647
Danny Evins <dcastkid@gmail.com>
2016-07-21 15:51:04 -0500

    form hides, working on show button

--- app/controllers/contacts.js
+++ app/controllers/contacts.js
@@ -2,6 +2,24 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
+    showForm() {
+      this.toggleProperty('formVisible');
+      this.toggleProperty('outputVisible')
+    },
+
+    deleteContact(contact) {
+      if (confirm('remove contact?')) {
+        fetch('https://tiny-tn.herokuapp.com/collections/dwe-people/' + contact._id, {
+          method: 'delete',
+        })
+        .then(() => {
+          this.set('model', this.model.filter((item) => {
+            return item._id !== contact._id;
+          }))
+        })
+      }
+    },
+
     addContact() {
       const data = {
         firstName: this.firstName,
@@ -17,12 +35,16 @@ export default Ember.Controller.extend({
         },
         method: 'post',
         body: JSON.stringify(data),
+      })
+      .then((res) => res.json())
+      .then((contact) => {
+        this.set('model', [contact, ...this.model]);
       });
 
-      Ember.set(this, firstName, '');
-      Ember.set(this, lastName = '');
-      Ember.set(this, address = '');
-      Ember.set(this, phoneNumber = '');
+      this.set(firstName, '');
+      this.set(lastName, '');
+      this.set(address, '');
+      this.set(phoneNumber, '');
     }
   }
 });
